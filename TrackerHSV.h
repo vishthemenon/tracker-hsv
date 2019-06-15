26d3cb6a982a8e8b052158d7dd5c698c52b3b77c
Vishnu R Menon <vishthemenon@gmail.com>
2019-06-16 00:53:45 +0100

    POC for hsv based detection

--- TrackerHSV.h
+++ TrackerHSV.h
@@ -17,11 +17,25 @@ using namespace std;
 using namespace cv;
 
 class TrackerHSV : public Tracker {
+  vector<Point2f> markerCorners, sortedCorners;
+  vector<Vec3d> objectCorners;
+  
+  Point2f computeIntersect(Vec2f line1, Vec2f line2);
+  
+  vector<Point2f> lineToPointPair(Vec2f line);
+  
+  bool acceptLinePair(Vec2f line1, Vec2f line2, double minTheta);
+  
+  void getObjectPoints(double markerLengthX, double markerLengthY);
+  
+  void sortCorners(vector<Point2f> &markerCorners, vector<Point2f> &sortedCorners);
 
 public:
-  TrackerHSV(CVCalibration& cvl, bool showFrame=true);
-  bool detectLandingPad(Mat& frame) override;
-  int getPose(Mat& frame, Vec3d& translationVec, Vec3d& rotationVec) override;
+  TrackerHSV(CVCalibration &cvl, bool showFrame = true);
+  
+  bool detectLandingPad(Mat &frame) override;
+  
+  int getPose(Mat &frame, Vec3d &translationVec, Vec3d &rotationVec) override;
 };
 
 #endif /* TrackerHSV_h */
